Corrected transpositions of Rotation matrices in get_angle_bearing and changed angle threshold to 10

src/continous_operations/triangulate_candidates.py
@@ -30,7 +30,7 @@ def filter_triangulated_points(points_3d, M1, M2, K, candidates, first_obs_candi
 def triangulate_points(state: State, current_R, current_t, K, triangulate_signal):
   # parameters to tune
   distance_threshold = 1
-  angle_treshold = 3
+  angle_treshold = 10
 
   current_pose = np.hstack((current_R, current_t[:,None]))
 

src/utils/utility_tools.py
@@ -51,12 +51,11 @@ def get_angle_bearing(current_points, prev_points, poses, current_pose, K):
      - angles_degrees: np.ndarray(N,) - Angles between corresponding vectors in degrees
    """
 
-    poses = np.reshape(poses, (12, poses.shape[2])).T
     num_points = prev_points.shape[1]
     K_inv = np.linalg.inv(K)
 
     # Get Rotation matrix of current pose
-    R_current = current_pose[:3, :3].T
+    R_current = current_pose[:3, :3]
 
     # Convert points to homogeneous coordinates
     current_points_homogeneous = np.vstack((current_points, np.ones((1, num_points))))
@@ -70,8 +69,8 @@ def get_angle_bearing(current_points, prev_points, poses, current_pose, K):
 
     for i in range(num_points):
         # Get R and t matrices of previous pose
-        t_prev = poses[i, :].reshape((3, 4))
-        R_prev = t_prev[:3, :3].T
+        T_prev = poses[:, :, i]
+        R_prev = T_prev[:3, :3]
 
         # De-rotate previous point to current camera frame
         deRotation = R_prev.T @ R_current